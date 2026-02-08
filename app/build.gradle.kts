9b4c7446e0db3d8e97372e185a63ef98ecf307be
Sairaj <sairajsawake@gmail.com>
2026-02-08 12:52:11 +0530

    My work before commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.play.services.auth)
 
     // Coroutines
     implementation(libs.kotlinx.coroutines.android)
